b147cd55fc71a526ad177c40bad1a24ebaa4b580
PlopesK <popi081004@hotmail.com>
2022-10-13 22:37:38 -0300

    Added even more animations

--- js/jquery.js
+++ js/jquery.js
@@ -1,3 +1,4 @@
+/*Navbar Mobile*/
 $(document).ready(function(){
   $("a").on('click', function(event) {
 
@@ -15,3 +16,20 @@ $(document).ready(function(){
     }
   });
 });
+
+/*Reveal text*/
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
